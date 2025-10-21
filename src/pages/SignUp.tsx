c82d2e6dda2326c46e30f273f47e144b283d4954
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-10-21 00:20:59 -0300

    feat: link to signUp and create SignUp

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,20 +2,31 @@ import { useState } from "react"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
-export function SignIn() {
+export function SignUp() {
+  const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [passwordConfirm, setPasswordConfirm] = useState("")
   const [isLoading, setIsLoading] = useState(false)
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault()
 
-    console.log(email, password)
+    console.log(name, email, password, passwordConfirm)
   }
 
 
   return (
     <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+
+      <Input
+        required
+        legend="nome"
+        type="text"
+        placeholder="Digite seu nome"
+        onChange={(e) => setName(e.target.value)}
+      />
+
       <Input
         required
         legend="E-mail"
@@ -30,10 +41,19 @@ export function SignIn() {
         type="password"
         placeholder="123456"
         onChange={(e) => setPassword(e.target.value)}
-
       />
 
-      <Button type="submit" isLoading={isLoading}>Entrar</Button>
+      <Input
+        required
+        legend="Confirme a Senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setPasswordConfirm(e.target.value)}
+      />
+
+      <Button type="submit" isLoading={isLoading}>Cadastrar</Button>
+
+      <a href="/" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease">Já tenho uma conta</a>
 
     </form>
   )
--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router"
 
 import { AuthLayout } from "../components/AuthLayout"
-import { SignIn } from "../pages/Sign"
+
+import { SignIn } from "../pages/Signin"
+import { SignUp } from "../pages/Signup"
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AuthLayout />}>
         <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Route>
     </Routes>
   )
