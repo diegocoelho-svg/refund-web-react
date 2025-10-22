990d1759d5f3aff85777742964b0d03f4ac04a6e
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-10-21 23:56:36 -0300

    feat: create select component and populate select with categories

---
+++ src/utils/categories.ts
@@ -0,0 +1,30 @@
+import foodSvg from "../assets/food.svg"
+import othersSvg from "../assets/others.svg"
+import servicesSvg from "../assets/services.svg"
+import transportSvg from "../assets/transport.svg"
+import accommodationSvg from "../assets/accommodation.svg"
+
+export const CATEGORIES = {
+  food: {
+    name: "Alimentação",
+    icon: foodSvg,
+  },
+  others: {
+    name: "Outros",
+    icon: othersSvg,
+  },
+  services: {
+    name: "Serviços",
+    icon: servicesSvg,
+  },
+  transportSvg: {
+    name: "Transporte",
+    icon: transportSvg,
+  },
+  accommodation: {
+    name: "Acomodação",
+    icon: accommodationSvg,
+  },
+}
+
+export const CATEGORIES_KEYS = Object.keys(CATEGORIES) as Array<keyof typeof CATEGORIES>
