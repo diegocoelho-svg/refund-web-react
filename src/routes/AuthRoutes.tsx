de053f0c4ff59c48a3d3ebe59cb6aaccd046968e
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-10-21 22:47:10 -0300

    feat: Create AppLayout

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -2,8 +2,8 @@ import { Routes, Route } from "react-router"
 
 import { AuthLayout } from "../components/AuthLayout"
 
-import { SignIn } from "../pages/Signin"
-import { SignUp } from "../pages/Signup"
+import { SignIn } from "../pages/SignIn"
+import { SignUp } from "../pages/SignUp"
 import { NotFound } from "../pages/NotFound"
 
 export function AuthRoutes() {
@@ -13,7 +13,7 @@ export function AuthRoutes() {
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Route>
-      
+
       <Route path="/*" element={<NotFound />} />
     </Routes>
   )
--- src/routes/EmployeeRoutes.tsx
+++ src/routes/EmployeeRoutes.tsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router"
 
 import { Refund } from "../pages/Refund"
 import { NotFound } from "../pages/NotFound"
+import { AppLayout } from "../components/AppLayout"
 
 export function EmployeeRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Refund />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/" element={<Refund />} />
+      </Route>
+        <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
