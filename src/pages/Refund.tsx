cfe0a84f163a6e00610d1bde8b7cfee08b8a01a1
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-10-22 00:21:19 -0300

    feat: Send Button

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -3,17 +3,29 @@ import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
 import { Input } from "../components/Input"
 import { Upload } from "../components/Upload"
 import { Select } from "../components/Select"
+import { Button } from "../components/Button"
 
 export function Refund() {
+  const [name, setName] = useState("")
+  const [amount, setAmount] = useState("")
   const [category, setCategory] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const [filename, setFilename] = useState<File | null>(null)
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
+
+    console.log(name, amount, category, filename)
+  }
+
   return (
-    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+    <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
       <header>
         <h1 className="text-xl font-bold text-gray-100">Solicitação de Reembolso</h1>
         <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso.</p>
       </header>
 
-      <Input required legend="Nome da solicitação" />
+      <Input required legend="Nome da solicitação" value={name} onChange={(e) => setName(e.target.value)} />
 
       <div className="flex gap-4">
         <Select required legend="categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
@@ -24,11 +36,12 @@ export function Refund() {
           ))}
         </Select>
 
-        <Input legend="valor" required />
+        <Input legend="valor" required value={amount} onChange={(e) => setAmount(e.target.value)} />
       </div>
 
-      <Upload />
+      <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e?.target.files[0])} />
 
+      <Button type="submit" isLoading={isLoading}>Enviar</Button>
     </form>
   )
 }
