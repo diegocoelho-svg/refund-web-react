eb234142cb2770cdb8cfa8e5af266bfce0bcec83
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-10-25 16:49:34 -0300

    feat: implement dynamic routing and session handling for authentication and reimbursements

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,14 +7,31 @@ import { EmployeeRoutes } from "./EmployeeRoutes"
 
 const isLoading = false
 
+const session = {
+  user: {
+    role: "employee",
+  }
+}
+
 export function Routes() {
-  if(isLoading) {
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />
+      case "manager":
+        return <ManagerRoutes />
+      default:
+        return <AuthRoutes />
+    }
+  }
+
+  if (isLoading) {
     return <Loading />
   }
-  
+
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <Route />
     </BrowserRouter>
   )
 }
