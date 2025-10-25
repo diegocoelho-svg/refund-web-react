50c48603e9466f5efe2f9e9851dee1c6ec3f5053
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-10-25 16:27:26 -0300

    feat: Refund only read

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,7 +7,7 @@ import { CATEGORIES } from "../utils/categories"
 
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
-import { RefundItem } from "../components/RefundItem"
+import { RefundItem, type RefundItemProps } from "../components/RefundItem"
 import { Pagination } from "../components/Pagination"
 
 const REFUND_EXAMPLE = {
@@ -22,6 +22,7 @@ export function Dashboard() {
   const [name, setName] = useState("")
   const [page, setPage] = useState(1)
   const [totalOfPage, setTotalOfPage] = useState(10)
+  const [refunds, setRefunds] = useState<RefundItemProps[]>([REFUND_EXAMPLE])
 
   function fetchRefunds(e: React.FormEvent) {
     e.preventDefault()
@@ -55,8 +56,8 @@ export function Dashboard() {
         </Button>
       </form>
 
-      <div className="mt-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll">
-        <RefundItem data={REFUND_EXAMPLE} />
+      <div className="my-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll">
+        {refunds.map((item) => (<RefundItem key={item.id} data={item} href={`/refund/${item.id}`} />))}
       </div>
 
       <Pagination
--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useNavigate } from "react-router"
+import { useNavigate, useParams } from "react-router"
 
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
 import { Input } from "../components/Input"
@@ -8,16 +8,21 @@ import { Select } from "../components/Select"
 import { Button } from "../components/Button"
 
 export function Refund() {
-  const [name, setName] = useState("")
-  const [amount, setAmount] = useState("")
-  const [category, setCategory] = useState("")
+  const [name, setName] = useState("Teste")
+  const [amount, setAmount] = useState("34")
+  const [category, setCategory] = useState("transport")
   const [isLoading, _setIsLoading] = useState(false)
   const [filename, setFilename] = useState<File | null>(null)
 
   const navigate = useNavigate()
+  const params = useParams<{ id: string }>()
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault()
+
+    if (params.id) {
+      return navigate(-1)
+    }
 
     console.log(name, amount, category, filename)
     navigate("/confirm", { state: { fromSubmit: true } })
@@ -30,10 +35,16 @@ export function Refund() {
         <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso.</p>
       </header>
 
-      <Input required legend="Nome da solicitação" value={name} onChange={(e) => setName(e.target.value)} />
+      <Input 
+        required 
+        legend="Nome da solicitação" 
+        value={name} 
+        onChange={(e) => setName(e.target.value)} 
+        disabled={!!params.id}
+      />
 
       <div className="flex gap-4">
-        <Select required legend="categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
+        <Select required legend="categoria" value={category} onChange={(e) => setCategory(e.target.value)} disabled={!!params.id}>
           {CATEGORIES_KEYS.map((category) => (
             <option key={category} value={category}>
               {CATEGORIES[category].name}
@@ -41,12 +52,14 @@ export function Refund() {
           ))}
         </Select>
 
-        <Input legend="valor" required value={amount} onChange={(e) => setAmount(e.target.value)} />
+        <Input legend="valor" required value={amount} onChange={(e) => setAmount(e.target.value)} disabled={!!params.id} />
       </div>
 
       <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e?.target.files[0])} />
 
-      <Button type="submit" isLoading={isLoading}>Enviar</Button>
+      <Button type="submit" isLoading={isLoading}>
+        {params.id ? "Voltar" : "Enviar"}
+      </Button>
     </form>
   )
 }
