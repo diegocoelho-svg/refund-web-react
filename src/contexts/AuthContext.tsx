2dd4932b1abb6ab74db1dabfc52ed6b1cde31ebd
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-10-28 20:41:14 -0300

    feat(auth): implement fully typed login flow with API integration and role-based routing

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import { createContext, type ReactNode } from "react";
 
-export const AuthContext = createContext({})
+type AuthContext = {
+  session: null | UserAPIResponse
+  save: (data: UserAPIResponse) => void
+}
+
+export const AuthContext = createContext({} as AuthContext)
 
 export function AuthProvider({ children }: { children: ReactNode }) {
+  const [session, setSession] = useState<null | UserAPIResponse>(null)
+
+  function save(data: UserAPIResponse) {
+    setSession(data)
+  }
+
   return (
-    <AuthContext.Provider value={{ name: "Diego" }}>
+    <AuthContext.Provider value={{ session, save }}>
       {children}
     </AuthContext.Provider>
   )
